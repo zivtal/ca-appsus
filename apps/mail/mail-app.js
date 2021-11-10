622f2de8c394f813d2f5604de04b463ddee55514
zivtal <zivtal83@gmail.com>
2021-11-10 11:11:58 +0200

    mail-app added

---
+++ apps/mail/mail-app.js
@@ -0,0 +1,14 @@
+export default {
+    props: [],
+    components: {},
+    template: ``,
+    data() {
+        return {}
+    },
+    created() { },
+    updated() { },
+    destroyed() { },
+    methods: {},
+    computed: {},
+    watch: {},
+}
