13f3826ae53cf00dd0c7fd3220b17e223345c89d
zivtal <zivtal83@gmail.com>
2021-11-10 13:04:58 +0200

    mail app update

--- pages/mail-app.cmp.js
+++ pages/mail-app.cmp.js
@@ -1,16 +1,94 @@
+import { utilService } from "../services/utils.service";
+
+const getFolder = {
+	props: ['folder'],
+	template: `<div @click="$emit('change',folder)">{{folder}}</div>`,
+}
+
+const folderList = {
+	props: ['folders'],
+	components: {
+		getFolder,
+	},
+	template: `
+		<section class="mail-folder flex columns">
+			<template v-for="folder in folders">
+				<get-folder :class="{selected: folder === active}" :folder="folder" @change="folderChange"></get-folder>
+			</template>
+		</section>
+	`,
+	data() {
+		return {
+			active: 'Inbox',
+		}
+	},
+	methods: {
+		folderChange(activeFolder) {
+			this.active = activeFolder;
+			console.log(this.active);
+		}
+	},
+}
+
+const mailPreview = {
+	props: ['mail'],
+	template: `
+		<section class="preview flex">
+			<div class="star">X</div>
+			<div class="content"><p>content</p></div>
+			<div class="date"><p>date</p></div>
+			<div class="controls flex">kkk</div>
+		</section>
+	`
+}
+
+const mailList = {
+	props: ['mails'],
+	components: {
+		mailPreview,
+	},
+	template: `
+		<section class="mail-list">
+			<section class="controls">controls</section>
+			<section class="list flex columns">
+			</section>
+		</section>
+	`,
+}
+
 export default {
 	props: [],
-	components: {},
+	components: {
+		folderList,
+	},
 	template: `
-        <h1>AppSus</h1>
+		<section class="mail-app flex">
+			<folder-list :folders="folders"/>
 
+			<section class="mail-list">
+				<section class="controls">controls</section>
+				<section class="list flex columns">
+					<section class="preview flex">
+						<div class="star">X</div>
+						<div class="content"><p>content</p></div>
+						<div class="date"><p>date</p></div>
+						<div class="controls flex">kkk</div>
+					</section>
+				</section>
+			</section>
+		</section>
     `,
 	data() {
-		return {};
+		return {
+			mails: null,
+			folders: ['All', 'Inbox', 'Sent', 'Drafts', 'Trash'],
+		};
 	},
-	created() {},
-	updated() {},
-	destroyed() {},
+	created() {
+
+	},
+	updated() { },
+	destroyed() { },
 	methods: {},
 	computed: {},
 	watch: {}
