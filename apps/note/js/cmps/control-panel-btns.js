efd372f9179f54792933c711a7917a2242c7fabd
amir <amirinbarcode@gmail.com>
2021-11-11 09:40:23 +0200

    ok

--- apps/note/js/cmps/control-panel-btns.js
+++ apps/note/js/cmps/control-panel-btns.js
@@ -1,4 +1,6 @@
 import { eventBus } from '../../../../services/event.bus-service.js';
+import addNewNote from './add-new-note.cmp.js';
+import { utilService } from '../../../../services/utils.service.js';
 
 export default {
 	props: [ 'note' ],
@@ -6,17 +8,19 @@ export default {
 	template: `
             <div class="note-toolbar">
                 <input type="color" v-model="backgroundColor" @blur="changeColor"></input>
-                <button> edit note </button>
+                <button @click="openEditor"> edit note </button>
+				<add-new-note v-if="isEditor" class="note-editor" style="opacity:100%" :editNote="note" />
                 <button> add label </button>
-                <button> duplicate note </button>
-                <button> delete note </button>
+                <button @click="duplicateNote"> duplicate note </button>
+                <button @click="removeNote"> delete note </button>
                 <button> Export to email </button>
             </div>
     `,
 	data() {
 		return {
 			currNote: this.note,
-			backgroundColor: ''
+			backgroundColor: '',
+			isEditor: false
 		};
 	},
 	created() {},
@@ -25,11 +29,24 @@ export default {
 	methods: {
 		changeColor() {
 			eventBus.$emit('showChange', { note: this.currNote, color: this.backgroundColor });
+		},
+		openEditor() {
+			console.log('s');
+			this.isEditor = true;
+		},
+		duplicateNote() {
+			const copyNote = utilService.deepCopy(this.note);
+			eventBus.$emit('copyNote', copyNote);
+			// const copyNote = JSON.parse(stringfl());
+		},
+		removeNote() {
+			eventBus.$emit('removenote', this.note);
 		}
 	},
 	computed: {},
 	watch: {},
 	components: {
-		eventBus
+		eventBus,
+		addNewNote
 	}
 };
