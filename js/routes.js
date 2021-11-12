0b7c13d0f0ffc8ffeb6a6afe4b6a3086c55c712d
zivtal <zivtal83@gmail.com>
2021-11-12 08:48:59 +0200

    change location

--- js/routes.js
+++ js/routes.js
@@ -1,6 +1,6 @@
-import homePage from '../../pages/home-page.cmp.js';
-import mailPage from '../../pages/mail-app.cmp.js';
-import notePage from '../../pages/note-app.cmp.js';
+import homePage from './pages/home-page.cmp.js';
+import mailPage from './pages/mail-app.cmp.js';
+import notePage from './pages/note-app.cmp.js';
 
 const routes = [
 	{
