88c7425709d028791141c057a0aab0cbb424d13b
zivtal <zivtal83@gmail.com>
2021-11-13 17:12:53 +0200

    homepage

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -9,7 +9,7 @@ export default {
 						AppSus Email-service' offers the most fast and qualified Email-Services these days, 
 						makes it easier for you to manage your emails. 
 					</div> 
-					<button class="default-button">Learn More</button>
+					<button class="default-button" @click="goToMail">Try it now</button>
 				</article>
 				<article class="box-app flex">
 					<img src="./img/keep.png" />
@@ -17,16 +17,27 @@ export default {
 						Manage your thoughts, and lists with AppSus' Keep-Service,
 						our service supports videos and pictures to be kept.
 					</div> 
-					<button class="default-button">Learn More</button>
+					<button class="default-button" @click="goToKeep">Try it now</button>
 				</article>
 				<article class="box-app flex">
 					<img src="./img/books.png">
 					<div>
 						Manage your books with AppSus' Books-Service!
 					</div>
-					<button class="default-button">Learn More</button>
+					<button class="default-button" @click="goToBook">Try it now</button>
 				</article>
 			</section>
 		</section>
     `,
+	methods: {
+		goToMail() {
+			this.$router.push({ path: `/mail` });
+		},
+		goToKeep() {
+			this.$router.push({ path: `/note` });
+		},
+		goToBook() {
+			this.$router.push({ path: `/book` });
+		},
+	}
 };
